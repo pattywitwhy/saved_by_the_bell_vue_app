b579ec7e5b9aa309dc82e8954b0600e77ee77de2
centerpush <dylancross100@gmail.com>
2019-03-01 16:15:17 -0600

    Tidied up the nav bar

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,33 @@
   <div id="app">
     <div id="nav">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <router-link class="navbar-brand" to="/">$aved by the Bell!</router-link>
+        <router-link class="navbar-brand" to="goals">$aved by the Bell!</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <router-link class="nav-link" to="#">Home <span class="sr-only">(current)</span></router-link>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="goals">Goals</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="#">My Profile</router-link>
+              <router-link class="nav-link" to="temptations">Temptations</router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="#">Goals</router-link>
+              <a class="nav-link" href="/graphs">Graphs</a>
             </li>
-            <li class="nav-item">
-
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Menu
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <router-link class="dropdown-item" to="/temptations">Temptations</router-link>
-                  <a class="dropdown-item" href="/graphs">Graphs</a>
-                </div>
-            </li>
-
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="login">Login</router-link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" on:click='logout()'>Logout</a>
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Account
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li class="dropdown-item">
+                  <router-link class="nav-link" to="login">Login</router-link>
+                </li>
+                <li class="dropdown-item">
+                  <a class="nav-link" on:click='logout()'>Logout</a>
+                </li>
+              </div>
             </li>
           </ul>
         </div>
--- src/router.js
+++ src/router.js
@@ -3,12 +3,8 @@ import Router from 'vue-router';
 import TemptationsIndex from './views/TemptationsIndex.vue';
 import TemptationsEdit from './views/TemptationsEdit.vue';
 import Graphs from './views/Graphs.vue';
-<<<<<<< HEAD
 import Login from './views/Login.vue';
-=======
 import Goals from './views/Goals.vue';
-
->>>>>>> 7bc87b54a7a96bcb2eae8f4caf8ff7564172096d
 
 Vue.use(Router)
 
@@ -19,10 +15,7 @@ export default new Router({
           { path: '/temptations', name: 'temptations-index', component: TemptationsIndex },
           { path: '/temptations/:id/edit', name: 'temptations-edit', component: TemptationsEdit },
           { path: '/graphs', name: 'graphs', component: Graphs },
-<<<<<<< HEAD
-          { path: '/login', name: 'login', component: Login }
-=======
+          { path: '/login', name: 'login', component: Login },
           { path: '/goals', name: 'goals', component: Goals }
->>>>>>> 7bc87b54a7a96bcb2eae8f4caf8ff7564172096d
           ]
 })
